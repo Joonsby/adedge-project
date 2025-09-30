81f7dc626c6c40e27819edb41d472dd6ff396369
Joonsup Lee <flamewizkey@naver.com>
2025-09-30 23:30:12 +0900

    Enhance Header component with animations and interactions
    - Add smooth slide-down animation for submenu (#all-submenu)
    - Implement preventDefault() for all navigation links except login
    - Improve CSS transitions and hover effects
    - Add responsive design considerations
    - Update Header.js with handleLinkClick function
    - Enhance Header.css with opacity and transform animations
    - Minor updates to Login.css and other style files

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -39,6 +39,10 @@ const Header = () => {
     setSidebarOpen(false);
   };
 
+  const handleLinkClick = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <header className="pof width-100 flex">
       <div className="logo dib">
@@ -48,59 +52,57 @@ const Header = () => {
       {!isMobile && (
         <>
           <nav 
-            className="dib poa width-60" 
+            className="dib poa width-80" 
             onMouseEnter={handleNavMouseEnter}
             onMouseLeave={handleNavMouseLeave}
           >
             <ul className="flex main-menu">
-              <li><a href="#" className="show">홈</a></li>
-              <li><a href="#" className="show">제품소개</a></li>
-              <li><a href="#" className="show">특장점</a></li>
-              <li><a href="#" className="show">가격정책</a></li>
-              <li><a href="#" className="show">광고신청</a></li>
+              <li><a href="#" className="show" onClick={handleLinkClick}>홈</a></li>
+              <li><a href="#" className="show" onClick={handleLinkClick}>제품소개</a></li>
+              <li><a href="#" className="show" onClick={handleLinkClick}>특장점</a></li>
+              <li><a href="#" className="show" onClick={handleLinkClick}>가격정책</a></li>
+              <li><a href="#" className="show" onClick={handleLinkClick}>광고신청</a></li>
             </ul>
           </nav>
 
-          {showSubmenu && (
-            <div 
-              id="all-submenu" 
-              className="poa width-100"
-              onMouseEnter={() => setShowSubmenu(true)}
-              onMouseLeave={handleNavMouseLeave}
-            >
-              <div className="flex width-60">
+          <div 
+            id="all-submenu" 
+            className={`poa width-100 ${showSubmenu ? 'show' : ''}`}
+            onMouseEnter={() => setShowSubmenu(true)}
+            onMouseLeave={handleNavMouseLeave}
+          >
+              <div className="flex width-80">
                 <ul className="width-100">
-                  <li className="por"><a href="#">회사개요</a></li>
-                  <li className="por"><a href="#">사업개요</a></li>
-                  <li className="por"><a href="#">조직구조</a></li>
-                  <li className="por"><a href="#">주요 협력사&고객사</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>회사개요</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>사업개요</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>조직구조</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>주요 협력사&고객사</a></li>
                 </ul>
                 <ul className="width-100">
-                  <li className="por"><a href="#">헬스케어솔루션</a></li>
-                  <li className="por"><a href="#">친환경스마트박스</a></li>
-                  <li className="por"><a href="#">IPTV/케이블광고</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>헬스케어솔루션</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>친환경스마트박스</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>IPTV/케이블광고</a></li>
                 </ul>
                 <ul className="width-100">
-                  <li className="por"><a href="#">차별화/경쟁력</a></li>
-                  <li className="por"><a href="#">성과/추진현황</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>차별화/경쟁력</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>성과/추진현황</a></li>
                 </ul>
                 <ul className="width-100">
-                  <li className="por"><a href="#">헬스케어솔루션 광고</a></li>
-                  <li className="por"><a href="#">친환경스마트박스 광고</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>헬스케어솔루션 광고</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>친환경스마트박스 광고</a></li>
                 </ul>
                 <ul className="width-100">
-                  <li className="por"><a href="#">헬스케어솔루션 광고</a></li>
-                  <li className="por"><a href="#">친환경스마트박스 광고</a></li>
-                  <li className="por"><a href="#">광고신청방법</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>헬스케어솔루션 광고</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>친환경스마트박스 광고</a></li>
+                  <li className="por"><a href="#" onClick={handleLinkClick}>광고신청방법</a></li>
                 </ul>
               </div>
             </div>
-          )}
 
           <ul id="right-menu" className="flex">
-            <li style={{display: 'none'}}><a href="#">MY페이지</a></li>
+            <li style={{display: 'none'}}><a href="#" onClick={handleLinkClick}>MY페이지</a></li>
             <li><a href="/login">로그인</a></li>
-            <li><a href="#">회원가입</a></li>
+            <li><a href="#" onClick={handleLinkClick}>회원가입</a></li>
           </ul>
         </>
       )}
